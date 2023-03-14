2bce780c51094fa3f90ae111d4852eccefb921da
Andrew Owino <andystudy4@gmail.com>
2023-03-14 22:12:06 +0300

    Add User to Comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_14_190208) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_14_191048) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,7 +19,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_14_190208) do
     t.bigint "project_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["project_id"], name: "index_comments_on_project_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "projects", force: :cascade do |t|
@@ -37,4 +39,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_14_190208) do
   end
 
   add_foreign_key "comments", "projects"
+  add_foreign_key "comments", "users"
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -7,14 +7,17 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  project_id :bigint           not null
+#  user_id    :bigint           not null
 #
 # Indexes
 #
 #  index_comments_on_project_id  (project_id)
+#  index_comments_on_user_id     (user_id)
 #
 # Foreign Keys
 #
 #  fk_rails_...  (project_id => projects.id)
+#  fk_rails_...  (user_id => users.id)
 #
 require 'rails_helper'
 
