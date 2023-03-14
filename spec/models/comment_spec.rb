43c5f7e77dae884e50798f4a14df36e6b04c8e0e
Andrew Owino <andystudy4@gmail.com>
2023-03-14 22:48:35 +0300

    Fix failing tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -25,7 +25,12 @@ RSpec.describe Comment, type: :model do
   describe 'validations' do
     it 'is valid with a text, project, and user' do
       project = Project.create(name: 'Test Project')
-      user = User.create(name: 'Test Project', email: "example@example.com", role: 1)
+      user = User.create(
+        name: 'Test Project', 
+        email: "example@example.com", 
+        role: 1, 
+        password: "word123pass", 
+        password_confirmation: "word123pass")
 
       comment = Comment.new(text: 'Test Project', project_id: project.id, user_id: user.id)
       expect(comment).to be_valid
@@ -33,7 +38,12 @@ RSpec.describe Comment, type: :model do
 
     it 'is invalid without a text' do
       project = Project.create(name: 'Test Project')
-      user = User.create(name: 'Test Project', email: "example@example.com", role: 1)
+      user = User.create(
+        name: 'Test Project', 
+        email: "example@example.com", 
+        role: 1, 
+        password: "word123pass", 
+        password_confirmation: "word123pass")
 
       comment = Comment.new(project_id: project.id, user_id: user.id)
       expect(comment).to_not be_valid
