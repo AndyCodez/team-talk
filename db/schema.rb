a353eebd1555d63b89515800b4e721a6031a2074
Andrew Owino <andystudy4@gmail.com>
2023-03-14 21:27:47 +0300

    Add Comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_14_180900) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_14_182614) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "text"
+    t.bigint "project_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_comments_on_project_id"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
@@ -20,4 +28,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_14_180900) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "projects"
 end
