495a25c126aeb710147825606569bb5e674cfcb1
Andrew Owino <andystudy4@gmail.com>
2023-03-14 21:53:39 +0300

    Validate presence of text field on comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -18,4 +18,5 @@
 #
 class Comment < ApplicationRecord
   belongs_to :project
+  validates :text, presence: true
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -19,5 +19,17 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'is valid with a text' do
+      project = Project.create(name: 'Test Project')
+      comment = Comment.new(text: 'Test Project', project_id: project.id)
+      expect(comment).to be_valid
+    end
+
+    it 'is invalid without a name' do
+      project = Project.create(name: 'Test Project')
+      comment = Comment.new(project_id: project.id)
+      expect(comment).to_not be_valid
+    end
+  end
 end
