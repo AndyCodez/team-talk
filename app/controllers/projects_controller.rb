c4e798805c4fbc7a4361623b5cf1b43edf3552fa
Andrew Owino <andystudy4@gmail.com>
2023-03-15 22:41:10 +0300

    Implement project crud actions

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,16 +9,40 @@ class ProjectsController < ApplicationController
     if project.save
       flash[:success] = "New project created successfully."
       redirect_to root_path
+    else
+      render :new
     end 
   end
 
   def index
+    @projects = Project.all
   end
 
   def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+
+    if @project.update(project_params)
+      flash[:success] = "Project #{@project.name} updated successfully."
+      redirect_to @project
+    else
+      render :edit
+    end 
   end
 
   def show
+    @project = Project.find(params[:id])
+  end
+
+  def destroy
+    project= Project.find(params[:id])
+    if project.destroy
+      flash[:success] = "Project #{project.name} deleted successfully."
+      redirect_to root_path      
+    end
   end
 
   private
