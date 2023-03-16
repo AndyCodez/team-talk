1acd4b95340d908efb448a3e250e36c27387d635
Andrew Owino <andystudy4@gmail.com>
2023-03-17 00:31:58 +0300

    Add js for responsive navbar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,29 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// Select the mobile menu button
+const mobileMenuButton = document.querySelector('button[aria-controls="mobile-menu"]');
+
+// Select the mobile menu
+const mobileMenu = document.getElementById('mobile-menu');
+
+// Add an event listener to the mobile menu button to toggle the mobile menu
+mobileMenuButton.addEventListener('click', function() {
+  const expanded = this.getAttribute('aria-expanded') === 'true' || false;
+  this.setAttribute('aria-expanded', !expanded);
+  mobileMenu.classList.toggle('hidden');
+});
+
+// Select the profile dropdown button
+const userMenuButton = document.getElementById('user-menu-button');
+
+// Select the profile dropdown menu
+const userMenu = document.getElementById('user-menu');
+
+// Add an event listener to the profile dropdown button to toggle the dropdown menu
+userMenuButton.addEventListener('click', function() {
+  const expanded = this.getAttribute('aria-expanded') === 'true' || false;
+  this.setAttribute('aria-expanded', !expanded);
+  userMenu.classList.toggle('hidden');
+});
