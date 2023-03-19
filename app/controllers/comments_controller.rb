93ef5309a9786c6903b24a0e32e99b2dd086868a
Andrew Owino <andystudy4@gmail.com>
2023-03-19 19:30:54 +0300

    Remove flash message upon commenting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,12 @@ class CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
 
     if @comment.save
-      flash[:success] = 'New comment created successfully.'
-      update_comments 
+      render turbo_stream: 
+              turbo_stream.replace(
+                "comments", 
+                partial: "projects/comments", 
+                object: @comment.project.comments
+                )
     else
       flash[:warning] = 'Failed to add comment. Please try again.'
       redirect_to projects_path(comment.project)
@@ -15,9 +19,5 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:text).merge(params.permit(:project_id, :user_id))
-  end
-
-  def update_comments
-    render turbo_stream: turbo_stream.replace("comments", partial: "projects/comments", object: @comment.project.comments)
   end
 end
